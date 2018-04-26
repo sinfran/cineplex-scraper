30a7e5b65a4bbfc69751cefa7fc97efc0dd55817
Frances Sin <frances.sin31@gmail.com>
2018-04-25 20:05:46 -0700

    User can now request data from multiple theatres

--- cineplex_scraper.py
+++ cineplex_scraper.py
@@ -20,40 +20,43 @@ theatres = [ "scotiabank-theatre-vancouver",
              "cineplex-cinemas-marine-gateway-and-vip",
              "cineplex-odeon-international-village-cinemas",
              "silverCity-riverport-cinemas" ]
-            
-print (Fore.BLUE + "\nCineplex Theatres in Vancouver:")
+
+print (Style.BRIGHT)
+print (Fore.BLUE + "Cineplex Theatres in Vancouver:")
 
 for index, theatre in enumerate(theatres, start = 1):
     print (Fore.RED + "[" + str (index) + "] " + theatre)
 print (Fore.BLUE);
 
 # Prompt user for theatre selection
-user_selection = input ("Select a theatre: " + Fore.RED)
+user_input = input ("Select theatres(s): " + Fore.GREEN)
+user_selections = list (map (int, user_input.replace (" ", "").split(',')))
 
 # Construct URL
 now = datetime.datetime.now()
 date = str (now.month) + '/' + str (now.day) + '/' + str (now.year)
-theatre = theatres[int (user_selection) - 1]
-url = 'https://www.cineplex.com/Showtimes/any-movie/' + theatre + '?Date=' + date
 
 
-# Open connection and download webpage
-context = ssl._create_unverified_context()
-client = urlopen (url, context=context)
-raw = client.read()
-client.close()
+for i in user_selections:
+    theatre = theatres[i - 1]
+    url = 'https://www.cineplex.com/Showtimes/any-movie/' + theatre + '?Date=' + date
 
+    # Open connection and download webpage
+    context = ssl._create_unverified_context()
+    client = urlopen (url, context=context)
+    raw = client.read()
+    client.close()
 
-# Parse HTML
-soup = BeautifulSoup (raw, "html.parser")
+    # Parse HTML
+    soup = BeautifulSoup (raw, "html.parser")
 
-cards = soup.findAll ("div", { "class":"h3 showtime-card--title" })
+    cards = soup.findAll ("div", { "class":"h3 showtime-card--title" })
 
-print (Fore.BLUE + "\n" + theatre.upper() + " is currently playing:")
+    print (Fore.BLUE + "\n" + theatre.upper() + " is currently playing:")
 
-for card in cards:
-    title = card.meta["content"]
-    print (Fore.YELLOW + str (title))
+    for card in cards:
+        title = card.meta["content"]
+        print (Fore.YELLOW + " > " + str (title))
 
 print ("")
 
